251f764e01f014ff5cb038e3a91de40815b0877b
Nikita Rydanov <56316351+nrydanov@users.noreply.github.com>
2023-04-24 01:45:20 +0400

    Сверточная нейронная сеть (#5)
    * Сбалансировал классы
    
    * Добавил загрузчик аудио
    
    * Закэшил memmap, теперь он не требует повторных вычислений
    
    * Переписал на Torch
    
    * Немного расширил модель, добавил косинусный отжиг
    
    * Получил 0.45 acc

---
+++ loaders.py
@@ -0,0 +1,147 @@
+import os.path as path
+import os
+import glob
+import pandas as pd
+import matplotlib.pyplot as plt
+import librosa
+import numpy as np
+
+from tqdm import tqdm
+from tempfile import mkdtemp, gettempdir
+from itertools import islice
+from dataclasses import dataclass
+
+from torch.utils.data import Dataset
+
+from utils import label2vec
+
+def get_audio_by_id(base_folder, id, sr=22050):
+  id = f'{id:06}'
+  subfolder = id[:3]
+  filename = f'{base_folder}/{subfolder}/{id}.mp3'
+  return librosa.load(filename, sr=sr)[0]
+
+@dataclass
+class AudioDataset(Dataset):
+  data: pd.DataFrame
+  sr: int = 22050
+  n_fft: int = 2048
+  hop_length: int = 512
+  win_length: int = n_fft
+  suffix: int = None
+  fmin: int = 50
+  fmax: int = 14000
+  data_dir: str = None
+  
+  x = None
+  y = None
+  dtemp = ".cache"
+  progress_path = None
+  
+  def retrieve_image(self, spec):
+    fig = plt.figure(frameon=False)
+    ax = fig.add_subplot(111)
+    ax.set_position([0, 0, 1, 1])
+    plt.axis('off')
+    plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+    fig.patch.set_alpha(0)
+    
+    librosa.display.specshow(spec, ax=ax, n_fft=self.n_fft,
+                             hop_length=self.hop_length,
+                             win_length=self.win_length,
+                             fmin=self.fmin, fmax=self.fmax,
+                             sr=self.sr)
+    fig.canvas.draw()
+    rgba_buf = fig.canvas.buffer_rgba()
+    (w,h) = fig.canvas.get_width_height()
+    rgba_arr = np.frombuffer(rgba_buf, dtype=np.uint8).reshape((h, w, 4))[:, :, :3]
+    plt.close()
+    
+    return rgba_arr / 255
+  
+  def __restore_state__(self):
+    try:
+      with open(self.progress_path, 'r') as f:
+        i = int(f.read())
+    except OSError:
+      i = 0
+    return i
+  
+  def __save_state__(self, i):
+    with open(self.progress_path, 'w') as f:
+      f.write(str(i))
+      
+  def unload(self):
+    self.x = np.memmap(path.join(self.cache_path, 'x.dat'),
+                       shape=(self.data.shape[0], 480, 640, 3),
+                       dtype=np.float32,
+                       mode='r+')
+    self.y = np.memmap(path.join(self.cache_path, 'y.dat'),
+                       shape=(self.data.shape[0], 8),
+                       dtype=np.float32,
+                       mode='r+')
+  
+  def __post_init__(self):
+    
+    self.cache_path = f"{self.dtemp}/genre-research_{self.suffix}"
+    self.progress_path = f'{self.cache_path}/.progress'
+    if path.exists(self.cache_path):
+      mode = 'r+'
+    else:
+      os.makedirs(self.cache_path)
+      mode = 'w+'
+  
+    self.x = np.memmap(path.join(self.cache_path, 'x.dat'),
+                       shape=(self.data.shape[0], 480, 640, 3),
+                       dtype=np.float32,
+                       mode=mode)
+    self.y = np.memmap(path.join(self.cache_path, 'y.dat'),
+                       shape=(self.data.shape[0], 8),
+                       dtype=np.float32,
+                       mode=mode)
+    
+    init_state = self.__restore_state__()
+    i = init_state
+    for index, row in tqdm(islice(self.data.iterrows(), init_state, None, 1),
+                           total=len(self.data), initial=init_state):
+      if i % 100 == 0:
+        self.__save_state__(i)
+      
+      try:
+        audio = get_audio_by_id(self.data_dir, index, self.sr)
+      except Exception:
+        self.x[i] = np.zeros((480, 640, 3))
+        self.y[i] = np.zeros(8)
+        i += 1
+        continue
+      audio = np.pad(audio, (0, max(self.sr * 31 - audio.shape[0], 0)))
+      spec = librosa.power_to_db(
+        librosa.feature.melspectrogram(y=audio, 
+                                       n_fft=self.n_fft,
+                                       hop_length=self.hop_length,
+                                       win_length=self.win_length,
+                                       fmin=self.fmin, fmax=self.fmax,
+                                       sr=self.sr))
+      
+      image = self.retrieve_image(spec)
+      self.x[i] = image
+      self.y[i] = label2vec(8)(int(row['track_genres']))
+      i += 1
+    
+    self.__save_state__(str(len(self.data)))
+    if init_state != self.data.shape[0] and i == self.data.shape[0]:
+      print("Shuffling after first initialization")
+      p = np.random.permutation(self.data.shape[0])
+      self.x[:], self.y[:] = self.x[p], self.y[p]
+      self.x.flush()
+      self.y.flush()
+      self.unload()
+    
+  def __len__(self):
+    return self.data.shape[0]
+  
+  def __getitem__(self, index):
+    x_cur = self.x[index]
+    y_cur = self.y[index]
+    
+    return np.swapaxes(x_cur, 0, 2), y_cur
---
+++ utils.py
@@ -0,0 +1,82 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import torch
+import os
+import matplotlib.pyplot as plt
+
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.utils.tensorboard import SummaryWriter
+
+def label2vec(n_classes):
+  def inner(label):  
+    v = np.zeros(n_classes)
+    v[label] = 1
+    return v
+  return inner
+
+def plot_csv(filepath, **kwargs):
+  df = pd.read_csv(filepath)
+  sns.lineplot(data=df, **kwargs)
+  plt.show()
+  
+def save_state(model, opt, epoch, losses, best_loss):
+  
+  os.makedirs('checkpoint', exist_ok=True)
+  pd.DataFrame(losses).to_csv('checkpoint/train_progress.csv', index_label='id', 
+                              index=False)
+  
+  state = model.state_dict()
+  state['epoch'] = epoch
+  state['best_loss'] = best_loss
+  
+  torch.save(opt.state_dict(), 'checkpoint/optimizer.pt')
+  torch.save(state, 'checkpoint/model.pt')
+  
+def restore_state(model, opt, path):
+  try:
+    dict = torch.load(f"{path}/model.pt")
+    opt.load_state_dict(torch.load(f"{path}/optimizer.pt"))
+    model.load_state_dict(dict, strict=False)
+    train_process = pd.read_csv('checkpoint/train_progress.csv').to_dict('list')
+    return model, opt, dict['epoch'], dict['best_loss'], train_process
+  except FileNotFoundError as e:
+    print(f"Couldn't load checkpoint: {e}")
+    return model, opt, -1, 1e18, {}
+  
+def init_tensorboard_writer(writer):
+  layout = {
+    "Performance": {
+        "loss": ["Multiline", ["loss/train", "loss/val"]],
+        "acc": ["Multiline", ["acc/train", "acc/val"]],
+    },
+  }
+  
+  writer.add_custom_scalars(layout)
+  
+  return writer
+  
+def add_tensorboard_record(writer, epoch, **dict):
+
+  writer.add_scalar('loss/train', dict['val_loss'], epoch)
+  writer.add_scalar('loss/val', dict['val_loss'], epoch)
+  writer.add_scalar('acc/train', dict['acc'], epoch)
+  writer.add_scalar('acc/val', dict['val_acc'], epoch)
+  writer.add_scalar('lr', dict['lr'], epoch)
+  
+def add_record(train_progress, writer, **dict):
+  for k, w in dict.items(): 
+    if k not in train_progress:
+      train_progress.update({k : []})
+    train_progress[k].append(w)
+
+  add_tensorboard_record(writer, **dict)
+
+class DecayingCosineAnnealingLR(CosineAnnealingLR):
+  def get_lr(self) -> float:
+    lr = super().get_lr()
+    decay_factor = np.exp([-0.05 * (self.last_epoch // (2 * self.T_max))])[0]
+    lr[0] *= decay_factor
+    return lr
+  
+  
