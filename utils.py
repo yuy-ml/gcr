58fce1e0757229beb128b22b01a3d7e79d60b215
Nikita Rydanov <wrongwayboyy@gmail.com>
2023-04-23 16:25:18 +0400

    Изменил параметры отжига

--- utils.py
+++ utils.py
@@ -74,7 +74,7 @@ def add_record(train_progress, writer, **dict):
 class DecayingCosineAnnealingLR(CosineAnnealingLR):
   def get_lr(self) -> float:
     lr = super().get_lr()
-    decay_factor = np.exp([-0.2 * (self.last_epoch // self.T_max)])[0]
+    decay_factor = np.exp([-0.05 * (self.last_epoch // (2 * self.T_max))])[0]
     lr[0] *= decay_factor
     return lr
   
